ccd3ff928c04b909be2ea6429795f2d2d4e01d86
Fagner86 <71980908+Fagner86@users.noreply.github.com>
2021-04-09 23:56:03 -0300

    Primeiro commit

---
+++ src/provaParteFiinal02/Baralho.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package provaParteFiinal02;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author Fagner
+ */
+public class Baralho {
+
+    private List<CartaBaralho> cartas;
+
+    public Baralho() {
+        this.cartas = this.geraBaralho();
+
+    }
+
+    public CartaBaralho puxarCarta() {
+        CartaBaralho carta = this.cartas.get(0);
+        this.cartas.remove(0);
+
+        return carta;
+    }
+
+    public List<CartaBaralho> puxarMao() {
+        List<CartaBaralho> mao = new ArrayList<>();
+
+        mao.add(this.puxarCarta());
+        mao.add(this.puxarCarta());
+        mao.add(this.puxarCarta());
+        mao.add(this.puxarCarta());
+        mao.add(this.puxarCarta());
+
+        return mao;
+    }
+
+    public List<CartaBaralho> geraBaralho() {
+        List<CartaBaralho> baralho = new ArrayList<>();
+
+        for (Naipe naipe : Naipe.values()) {
+            for (Carta carta : Carta.values()) {
+                
+                
+               
+                // Fiz em outra pasta e deu certo mas ai quando copiei para a pasta do repositório  para colocar no git fica esse erro 
+                //
+               //Cannot invoke "java.net.URL.toExternalForm()" because "location" is null  
+                
+              URL caminhoImagem = getClass().getClassLoader().getResource(carta.getPontos() + naipe.getNome() + ".png" );
+                
+                // URL caminhoImagem = getClass().getClassLoader().getResource("imagens" );
+                baralho.add(new CartaBaralho(carta, naipe, new ImageIcon(caminhoImagem)));
+            }
+        }
+
+        Collections.shuffle(baralho);
+
+        return baralho;
+
+    }
+
+}
---
+++ src/provaParteFiinal02/Naipe.java
@@ -0,0 +1,23 @@
+
+package provaParteFiinal02;
+
+/**
+ *
+ * @author Fagner
+ */
+public enum Naipe {
+    
+    PAUS("paus"), OURO("ouro"), COPAS("copas"), ESPADA("espada");
+    
+    private final String nome;  
+
+    public String getNome() {
+        return nome;
+    }
+
+    private Naipe(String nome) {
+        this.nome = nome;
+    }
+ 
+    
+}
